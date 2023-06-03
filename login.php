e58de92f5aa54131d959518b8af8475054a6d82b
Alagarmuthu <134996536+Alagarmuthu@users.noreply.github.com>
2023-06-02 22:31:23 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<title>LOGIN PAGE</title>
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row"></div>
+		<div class="col-2"></div>
+		<div class="col-8" style="background: lovender;">
+			<form method="post"  action="">
+				<h1 align="center" style="color: bule;"><strong>LOGIN PAGE</strong></h1>
+
+				<div class="form-group">
+                 <label class="form label"><b>username</b></label>
+                 <input type="text" class="form-control" name="uname" placeholder="enter your username">
+				</div>
+
+				<div class="form-group">
+					<label class="form label"><b>password</b></label>
+					<input type="text" class="form-control" name="pwd" placeholder="enter your password">
+				</div>
+
+				<div class="form-group">
+					<input type="submit" class="btn-btn-success" name="submit" value="submit">
+					<input type="reset" class="btn-btn-dager" name ="reset" value="reset">
+				</div>
+			</form>
+		</div>
+			<div class="col-2">
+			
+		</div>
+	</div>
+</div>
+</body>
+</html>
